eef38f8e386408677f8d8dd4db2eb4c413af1d50
parvezmullah <mullahparvez@gmail.com>
2021-07-31 20:17:19 +0530

    docker compose- django-mysql

--- Docker/django_sample/django_sample/settings.py
+++ Docker/django_sample/django_sample/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'NAME': 'mydb',
         'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
