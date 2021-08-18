358b2b6591605da02fa09289d2721ac8b4dd1957
parvezmullah <mullahparvez@gmail.com>
2021-08-18 16:27:52 +0530

    golang routine

--- Golang/6-channel-and-go-routines/1-website-checker.go
+++ Golang/6-channel-and-go-routines/1-website-checker.go
@@ -1,24 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
 	sites := []string{
-		"https://github.com/",
+		"https://doxper.com",
+		"https://google.com/",
 		"https://github.com/",
 		"https://amazon.com",
-		"https://doxper.com"
+		// "https://parvez-close.com",
 	}
-	for _, site := range sites{
-		siteChecker(site)
+	c := make(chan string)
+	for _, site := range sites {
+		go siteChecker(site, c)
+	}
+
+	// for i := 0; i < len(sites); i++ {
+	// 	fmt.Println(<-c)
+
+	// }
+
+	for l := range c {
+		go func() {
+			time.Sleep(time.Second * 5)
+			siteChecker(l, c)
+		}()
 	}
 }
 
-func siteChecker(site string) {
-	response, err := http.Get(site)
-	if err != nil{
+func siteChecker(site string, c chan string) {
+	_, err := http.Get(site)
+	if err != nil {
 		fmt.Println(site, "is not up!")
+		c <- site
 		return
 	}
 	fmt.Println(site, "is up!")
+	c <- site
 }
